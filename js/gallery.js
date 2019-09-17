dbb96b3810b89cd3b8640d73d8a7fcdd7dc7426b
mallen47 <mallen@collab.net>
2019-09-17 13:32:05 -0400

    sync thumbnail images and alt text with main image

--- js/gallery.js
+++ js/gallery.js
@@ -1,3 +1,29 @@
 function activateGallery() {
-  alert("Hello from the gallery file!");
+	let thumbNails = document.querySelectorAll("#gallery-thumbs > div > img");
+	let mainImage = document.querySelector("#gallery-photo > img");
+
+	thumbNails.forEach(function(thumbNail) { 
+		thumbNail.addEventListener("click", function() {
+			mainImage.setAttribute("src", thumbNail.dataset.largeVersion);
+			mainImage.setAttribute("alt", thumbNail.dataset.title);
+		});
+	});
+
+
+	// let thumbNails = document.querySelectorAll("#gallery-thumbs > div > img");
+ //  let mainImage = document.querySelector("#gallery-photo > img");
+
+ //  thumbNails.forEach(function(thumbNail) {
+ //  	thumbNail.addEventListener("click", function() {
+ //  		let newImageSource = thumbNail.dataset.largeVersion;
+ //  		let newImageAlt = thumbNail.dataset.title;
+ //  		mainImage.setAttribute("src", newImageSource);
+ //  		mainImage.setAttribute("alt", newImageAlt);
+ //  	})
+ //  })
 }
+
+
+
+
+
